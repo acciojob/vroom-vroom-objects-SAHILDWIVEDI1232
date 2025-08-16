db1cfb70c17f810350dd375cd93f330101e02865
SAHILDWIVEDI1232 <168732444+SAHILDWIVEDI1232@users.noreply.github.com>
2025-08-16 12:17:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 // Complete the js code
-function Car(make, model) {}
-
-function SportsCar(make, model, topSpeed) {}
+function Car(make, model) {
+	
+getMakeModel(){
+return(this.make+"Toyota"+this.model+"Corolla");
+}
+}
+function SportsCar extends Car(make, model, topSpeed) {
+	getTopSpeed(){
+		return(this.make+"Toyota"+this.model+"Corolla"+this.topSpeed+"50mph");
+	}
+}
 
 // Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
+
+console.log(car.getMakeModel());
+console.log(car.getTopSpeed());
