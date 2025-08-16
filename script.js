a71d3537e4e04c39d04ac24a0221f8363d01f966
SAHILDWIVEDI1232 <168732444+SAHILDWIVEDI1232@users.noreply.github.com>
2025-08-16 12:27:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,37 @@
-// Complete the js code
+// Car constructor function
 function Car(make, model) {
-	this.make = make;
+  this.make = make;
   this.model = model;
 }
-getMakeModel(){
-	return(this.make+"Toyota"+this.model+"Corolla");
-}
-}
+
+// Add getMakeModel to Car prototype
+Car.prototype.getMakeModel = function () {
+  return this.make + " " + this.model;
+};
+
+// SportsCar constructor function
 function SportsCar(make, model, topSpeed) {
-	 this.topSpeed = topSpeed;}
+  // Call Car constructor
+  Car.call(this, make, model);
 
-	getTopSpeed(){
-		return(this.make+"Toyota"+this.model+"Corolla"+this.topSpeed+"50mph");
-	}
+  // Add topSpeed
+  this.topSpeed = topSpeed;
+}
 
+// Inherit from Car
+SportsCar.prototype = Object.create(Car.prototype);
+SportsCar.prototype.constructor = SportsCar;
+
+// Add getTopSpeed to SportsCar prototype
+SportsCar.prototype.getTopSpeed = function () {
+  return this.topSpeed;
+};
 
 // Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
 
+// Test example
 const car = new SportsCar("Ferrari", "Testarossa", 200);
 console.log(car.getMakeModel());  // Output: Ferrari Testarossa
-console.log(car.getTopSpeed());  
-
-
-
-
+console.log(car.getTopSpeed());   // Output: 200
